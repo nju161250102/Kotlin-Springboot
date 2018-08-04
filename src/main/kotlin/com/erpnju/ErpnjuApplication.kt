ae588f4c5de51ba4258cb925cbf3287078664821
161250102 <161250102@smail.nju.edu.cn>
2018-08-04 15:51:37 +0800

    spring boot 初始化 Web + JPA + SQL Server

---
+++ src/main/kotlin/com/erpnju/ErpnjuApplication.kt
@@ -0,0 +1,11 @@
+package com.erpnju
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ErpnjuApplication
+
+fun main(args: Array<String>) {
+    runApplication<ErpnjuApplication>(*args)
+}
